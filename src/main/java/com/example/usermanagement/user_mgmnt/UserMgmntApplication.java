8012c1dfd5d834f9f80f8dc77f81902ea73a0229
mahbub <elahidhk@gmail.come>
2025-02-15 08:12:17 -0500

    Initial Commit

---
+++ src/main/java/com/example/usermanagement/user_mgmnt/UserMgmntApplication.java
@@ -0,0 +1,13 @@
+package com.example.usermanagement.user_mgmnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgmntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMgmntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/usermanagement/user_mgmnt/UserMgmntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usermanagement.user_mgmnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMgmntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
